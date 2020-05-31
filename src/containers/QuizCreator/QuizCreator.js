bd65573849640e783cf745a970d5d8437cb7d543
L Burkova <burkova@xsud.ru>
2020-05-31 14:56:05 +0500

    router

---
+++ src/containers/QuizCreator/QuizCreator.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import { render } from '@testing-library/react';
+
+export default class QuizCreator extends Component {
+    render(){
+        return(
+            <div>
+                <h1>Quiz Creator</h1>
+            </div>
+        )
+    }
+}
